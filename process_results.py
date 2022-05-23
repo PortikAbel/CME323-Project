08a8ee3337731a33eabdf2bde3b99c9fbd88b458
PortikAbel <portikabel@yahoo.com>
2022-05-23 22:18:50 +0300

    visualized by boxplots

--- process_results.py
+++ process_results.py
@@ -1,34 +1,36 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-import matplotlib.axes as ax
-#import sys
+from graph_types import *
 
-seq_file = "results/seq_results.npy"
-par_file = "results/par_results.npy"
+SEQ = 'seq'
+PAR = 'par'
 
-seq = np.load(seq_file)
-par = np.load(par_file)
-#seq_sparse = np.load("sparse_seq_results.npy")
-#par_sparse = np.load("sparse_par_results.npy")
+def load_result(graph_type, algo_type):
+  return np.load(f'results/{graph_type}_{algo_type}.npy')
 
-print("seq\n", seq)
-print("par\n", par)
+def main():
+  n_list = [20, 50, 100, 150, 200]
+  d_list = [0.1, 0.3, 0.5, 0.7, 0.9]
+  n_rows = len(d_list)
+  n_cols = 2
+  n_boxes = len(n_list)
 
+  for fig_index, graph_type in enumerate([ERDOS_RENYI, BARABASI_ALBERT]):
+    fig = plt.figure(fig_index)
+    fig.suptitle(graph_type)
+    axs = fig.subplots(n_rows, n_cols)
 
-n = [20,50, 100, 150, 200]
-#plt.semilogy(n, seq_sparse[0], 'm^:', label="Seq, d=0.1")
-plt.semilogy(n, seq[0], 'm^-.', label="Seq, d=0.3")
-plt.semilogy(n, seq[1], 'm^--', label="Seq, d=0.5") 
-plt.semilogy(n, seq[2], 'm^--', label="Seq, d=0.7")
-plt.semilogy(n, seq[3], 'm^-', label="Seq, d=0.9")
-#plt.semilogy(n, par_sparse[0], 'co:', label="Par, d=0.1")
-plt.semilogy(n, par[0], 'co-.', label="Par, d=0.3")
-plt.semilogy(n, par[1], 'co-.', label="Par, d=0.5")
-plt.semilogy(n, par[2], 'co--', label="Par, d=0.7")
-plt.semilogy(n, par[3], 'co-', label="Par, d=0.9")
-plt.axis([10,210,0,750])
-plt.ylabel('Time')
-plt.xlabel('Number of Vertices')
-plt.title('Sequential vs Parallel Blossom Performance')
-plt.legend(loc='lower right')
-plt.show()
+    for column_index, algo_type in enumerate([SEQ, PAR]):
+      result = load_result(graph_type, algo_type)
+      for row_index, result_by_density in enumerate(result):
+        ax = axs[row_index][column_index]
+        ax.boxplot(result_by_density)
+        ax.set_title(f'{algo_type}: {d_list[row_index]}')
+        ax.set_yscale('log')
+        ax.set_xticks(list(range(1, n_boxes+1)))
+        ax.set_xticklabels(n_list)
+
+  plt.show()
+
+if __name__ == "__main__":
+  main()

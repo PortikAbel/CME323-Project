ad7b034321559f0c5e2dbb061695ebb3f1d7cbeb
PortikAbel <portikabel@yahoo.com>
2022-05-31 21:30:21 +0300

    showing only by one density at once

--- process_results.py
+++ process_results.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+from matplotlib.widgets import RadioButtons
 from graph_types import *
 
 SEQ = 'seq'
@@ -8,29 +9,38 @@ PAR = 'par'
 def load_result(graph_type, algo_type):
   return np.load(f'results/{graph_type}_{algo_type}.npy')
 
-def main():
-  n_list = [20, 50, 100, 150, 200]
-  d_list = [0.1, 0.3, 0.5, 0.7, 0.9]
-  n_rows = len(d_list)
-  n_cols = 2
+def main(graph_type):
+  n_list = ['20', '50', '100', '150', '200']
+  d_list = ['0.1', '0.3', '0.5', '0.7', '0.9']
   n_boxes = len(n_list)
 
-  for fig_index, graph_type in enumerate([ERDOS_RENYI, BARABASI_ALBERT]):
-    fig = plt.figure(fig_index)
-    fig.suptitle(graph_type)
-    axs = fig.subplots(n_rows, n_cols)
+  fig = plt.figure()
+  fig.suptitle(graph_type)
+  axs = fig.subplots(1, 3)
+  axs[0].sharey(axs[1])
 
+  result = []
+  for algo_type in [SEQ, PAR]:
+    result.append(load_result(graph_type, algo_type))
+
+  def redraw_results_with_density(d):
     for column_index, algo_type in enumerate([SEQ, PAR]):
-      result = load_result(graph_type, algo_type)
-      for row_index, result_by_density in enumerate(result):
-        ax = axs[row_index][column_index]
-        ax.boxplot(result_by_density)
-        ax.set_title(f'{algo_type}: {d_list[row_index]}')
-        ax.set_yscale('log')
-        ax.set_xticks(list(range(1, n_boxes+1)))
-        ax.set_xticklabels(n_list)
+      ax = axs[column_index]
+      ax.set_yscale('linear')
+      ax.cla()
+      ax.boxplot(result[column_index][d_list.index(d)])
+      ax.set_yscale('log')
+      ax.set_title(f'{algo_type}: d={d}')
+      ax.set_xticks(list(range(1, n_boxes+1)))
+      ax.set_xticklabels(n_list)
+      plt.draw()
+
+  redraw_results_with_density(d_list[0])
+
+  radio_button = RadioButtons(axs[2], d_list)
+  radio_button.on_clicked(redraw_results_with_density)
 
   plt.show()
 
 if __name__ == "__main__":
-  main()
+  main(BARABASI_ALBERT)

505556b04250bb1be7b2fc2f722c27353458c7ff
PortikAbel <portikabel@yahoo.com>
2022-06-02 16:03:32 +0300

    option for runtime ratio boxplots

--- process_results.py
+++ process_results.py
@@ -1,3 +1,4 @@
+from sys import argv
 import numpy as np 
 import matplotlib.pyplot as plt
 from matplotlib.widgets import RadioButtons
@@ -9,15 +10,18 @@ PAR = 'par'
 def load_result(graph_type, algo_type):
   return np.load(f'results/{graph_type}_{algo_type}.npy')
 
-def main(graph_type):
+def main(graph_type, seq_par_separated):
   n_list = ['20', '50', '100', '150', '200']
   d_list = ['0.1', '0.3', '0.5', '0.7', '0.9']
   n_boxes = len(n_list)
 
   fig = plt.figure()
   fig.suptitle(graph_type)
-  axs = fig.subplots(1, 3)
-  axs[0].sharey(axs[1])
+  if seq_par_separated:
+    axs = fig.subplots(1, 3)
+    axs[0].sharey(axs[1])
+  else:
+    axs = fig.subplots(1, 2)
 
   result = []
   for algo_type in [SEQ, PAR]:
@@ -35,12 +39,48 @@ def main(graph_type):
       ax.set_xticklabels(n_list)
       plt.draw()
 
-  redraw_results_with_density(d_list[0])
+  def redraw_result_comparision_with_density(d):
+    ax = axs[0]
+    ax.set_yscale('linear')
+    ax.cla()
+    ax.boxplot(result[0][d_list.index(d)] / result[1][d_list.index(d)])
+    ax.set_yscale('log')
+    ax.set_title(f'(sequential runtime) / (parallel runtime): d={d}')
+    ax.set_xticks(list(range(1, n_boxes+1)))
+    ax.set_xticklabels(n_list)
+    plt.draw()
 
-  radio_button = RadioButtons(axs[2], d_list)
-  radio_button.on_clicked(redraw_results_with_density)
+  if seq_par_separated:
+    redraw_results_with_density(d_list[0])
+    radio_button = RadioButtons(axs[2], d_list)
+    radio_button.on_clicked(redraw_results_with_density)
+  else:
+    redraw_result_comparision_with_density(d_list[0])
+    radio_button = RadioButtons(axs[1], d_list)
+    radio_button.on_clicked(redraw_result_comparision_with_density)
 
   plt.show()
 
 if __name__ == "__main__":
-  main(BARABASI_ALBERT)
+  if len(argv) < 3:
+    print('Missing arguments')
+    print(f'Usage: {argv[0]} er|ba  sep|comp')
+    exit()
+
+  if 'er' == argv[1]:
+    graph_type = ERDOS_RENYI
+  elif 'ba' == argv[1]:
+    graph_type = BARABASI_ALBERT
+  else:
+    print(f'Usage: {argv[0]} er|ba  sep|comp')
+    exit()
+
+  if 'sep' == argv[2]:
+    separated = True
+  elif 'comp' == argv[2]:
+    separated = False
+  else:
+    print(f'Usage: {argv[0]} er|ba sep|comp')
+    exit()
+
+  main(graph_type, separated)

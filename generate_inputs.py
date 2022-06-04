9083432812a8d81631e197d63d647b2cedd25301
PortikAbel <portikabel@yahoo.com>
2022-06-04 08:40:28 +0300

    Squashed commit of the following:
    commit 4b07eed7df83979fb77bb50285ec877e1be45d77
    Author: PortikAbel <portikabel@yahoo.com>
    Date:   Sat Jun 4 07:50:33 2022 +0300
    
        vasiualizing runtimes on barabasi-albert graphs
    
    commit 386261802346f085e3ee74101364a5d740a67a59
    Author: PortikAbel <portikabel@yahoo.com>
    Date:   Sat Jun 4 01:18:41 2022 +0300
    
        running on barabasi-albert graphs with different initial graphs
    
    commit 4ee35c5061bbfdd6d7712a4371f0db899e6b8301
    Author: PortikAbel <portikabel@yahoo.com>
    Date:   Fri Jun 3 16:31:00 2022 +0300
    
        generating barabasi-albert graphs from star and complete graphs

--- generate_inputs.py
+++ generate_inputs.py
@@ -1,15 +1,42 @@
+from math import sqrt
 import networkx as nx
 from os import makedirs
 from graph_types import *
 
-def generate_barabasi_albert(n, d):
-  m = round((n*(n-1)*d) / (2*(n+1)))
-  return nx.barabasi_albert_graph(n, m)
+class BarabasiAlbertGenerationException(nx.NetworkXError):
+  def __init__(self, n, d, initial_type, *args, **kw):
+    msg = f'Barabasi Albert graph with {n} nodes {d} density and initial {initial_type} graph is not possible.'
+    exception_message = msg
+    superinit = super().__init__
+    superinit(self, exception_message, *args, **kw)
 
-generator = {
-  ERDOS_RENYI: nx.erdos_renyi_graph,
-  BARABASI_ALBERT: generate_barabasi_albert,
-}
+def m_star_1(n, d):
+  return round((n - sqrt((1-2*d)*n**2+2*d*n)) / 2)
+
+def m_star_2(n, d):
+  return round((n + sqrt((1-2*d)*n**2+2*d*n)) / 2)
+
+def m_complete(n, d):
+  return round((2*n - 1 - sqrt((2*n-1)**2 - 4*d*n*(n+1))) / 2)
+
+def generate_barabasi_albert(n, d, initial_graph_type, m_formula_index=None):
+  assert initial_graph_type in ['star', 'complete']
+  if initial_graph_type == 'star':
+    assert m_formula_index in [1, 2]
+    if d > 0.5 and n > 1 + 1/(2*d-1):
+      raise BarabasiAlbertGenerationException(n, d, initial_graph_type)
+    if m_formula_index == 1:
+      m = m_star_1(n, d)
+    elif m_formula_index == 2:
+      m = m_star_1(n, d)
+    return nx.barabasi_albert_graph(n, m)
+  elif initial_graph_type == 'complete':
+    if n < (1 + d + sqrt(d*(d+3))) / 2*(1-d):
+      raise BarabasiAlbertGenerationException(n, d, initial_graph_type)
+    if n < (16-d) / d:
+      raise BarabasiAlbertGenerationException(n, d, initial_graph_type)
+    m = m_complete(n, d)
+    return nx.barabasi_albert_graph(n, m, None, nx.complete_graph(m))
 
 def main(CURRENT_TYPE):
   n_list = [20, 50, 100, 150, 200]
@@ -20,8 +47,23 @@ def main(CURRENT_TYPE):
   for i in range(niter):
     for n in n_list:
       for d in d_list:
-        G = generator[CURRENT_TYPE](n, d)
-        nx.write_adjlist(G, f"inputs/{CURRENT_TYPE}/n{n}_d{d}_{i}.adjlist")
+        if CURRENT_TYPE == ERDOS_RENYI:
+          G = nx.erdos_renyi_graph(n, d)
+          nx.write_adjlist(G, f"inputs/{CURRENT_TYPE}/n{n}_d{d}_{i}.adjlist")
+        elif CURRENT_TYPE == BARABASI_ALBERT:
+          initial = 'star' #'complete'
+          for edge_number_formula in [1, 2]:
+            try:
+              G = generate_barabasi_albert(n, d, initial, edge_number_formula)
+              nx.write_adjlist(G, f"inputs/{CURRENT_TYPE}/n{n}_d{d}_{initial}_{edge_number_formula}_{i}.adjlist")
+            except BarabasiAlbertGenerationException:
+              pass
+          initial = 'complete'
+          try:
+            G = generate_barabasi_albert(n, d, initial, edge_number_formula)
+            nx.write_adjlist(G, f"inputs/{CURRENT_TYPE}/n{n}_d{d}_{initial}_{i}.adjlist")
+          except BarabasiAlbertGenerationException:
+            pass
 
 if __name__ == "__main__":
   main(ERDOS_RENYI)

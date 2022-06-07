d7361f77c1161a15ac05996235172a49863e20fe
PortikAbel <portikabel@yahoo.com>
2022-06-07 08:31:03 +0200

    separated results and ratios moved to one figure

--- process_results.py
+++ process_results.py
@@ -10,18 +10,15 @@ PAR = 'par'
 def load_result(graph_type, algo_type):
   return np.load(f'results/{graph_type}_{algo_type}.npy')
 
-def main(graph_type, seq_par_separated):
+def main(graph_type):
   n_list = ['20', '50', '100', '150', '200']
   d_list = ['0.1', '0.3', '0.5', '0.7', '0.9']
   n_boxes = len(n_list)
 
   fig = plt.figure()
   fig.suptitle(graph_type)
-  if seq_par_separated:
-    axs = fig.subplots(1, 3)
-    axs[0].sharey(axs[1])
-  else:
-    axs = fig.subplots(1, 2)
+  axs = fig.subplots(1, 4)
+  axs[0].sharey(axs[1])
 
   result = []
   for algo_type in [SEQ, PAR]:
@@ -37,10 +34,7 @@ def main(graph_type, seq_par_separated):
       ax.set_title(f'{algo_type}: d={d}')
       ax.set_xticks(list(range(1, n_boxes+1)))
       ax.set_xticklabels(n_list)
-      plt.draw()
-
-  def redraw_result_comparision_with_density(d):
-    ax = axs[0]
+    ax = axs[2]
     ax.set_yscale('linear')
     ax.cla()
     ax.boxplot((result[0][d_list.index(d)] / result[1][d_list.index(d)]).transpose())
@@ -48,24 +42,19 @@ def main(graph_type, seq_par_separated):
     ax.set_title(f'(sequential runtime) / (parallel runtime): d={d}')
     ax.set_xticks(list(range(1, n_boxes+1)))
     ax.set_xticklabels(n_list)
+
     plt.draw()
 
-  if seq_par_separated:
-    redraw_results_with_density(d_list[0])
-    axs[2].set_title('Select density')
-    radio_button = RadioButtons(axs[2], d_list)
-    radio_button.on_clicked(redraw_results_with_density)
-  else:
-    redraw_result_comparision_with_density(d_list[0])
-    axs[1].set_title('Select density')
-    radio_button = RadioButtons(axs[1], d_list)
-    radio_button.on_clicked(redraw_result_comparision_with_density)
+  redraw_results_with_density(d_list[0])
+  axs[3].set_title('Select density')
+  radio_button = RadioButtons(axs[3], d_list)
+  radio_button.on_clicked(redraw_results_with_density)
 
   plt.show()
 
 if __name__ == "__main__":
-  usage = f'''Usage: {argv[0]} graph_type visualization_method'''
-  if len(argv) < 3:
+  usage = f'''Usage: {argv[0]} graph_type'''
+  if len(argv) < 2:
     print('Missing arguments')
     print(usage)
     exit()
@@ -82,12 +71,4 @@ if __name__ == "__main__":
     print(usage)
     exit()
 
-  if 'sep' == argv[2]:
-    separated = True
-  elif 'comp' == argv[2]:
-    separated = False
-  else:
-    print(f'Usage: {argv[0]} er|ba sep|comp')
-    exit()
-
-  main(graph_type, separated)
+  main(graph_type)
